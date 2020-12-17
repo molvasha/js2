08d2ecbd3eee97d1d0b609929d5cfb1719a16010
Valeriya <molvasha@gmail.com>
2020-12-17 17:16:13 +0300

    Домашняя работа 1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
+];
+//Функция для формирования верстки каждого товара
+const renderProduct = (title='товар', price=0) => {
+    return `<div class="product-item">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+//     const productsList = list.map(item => renderProduct(item.title, item.price));
+//     //console.log(productsList);
+//     document.querySelector('.products').innerHTML = productsList;
+// };
+    list.forEach(function (item) {
+    document.querySelector('.products').innerHTML += renderProduct(item.title, item.price)
+  })
+};
+
+
+renderPage(products);
+
